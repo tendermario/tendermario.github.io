6af7aea42c4c66686e67e3ca1ff7bca782f7d76e
Mario Viens <marviens@gmail.com>
2018-01-10 19:49:33 -0800

    updating index page - heavyvis examples

--- js/custom.js
+++ js/custom.js
@@ -5,12 +5,4 @@ $(function() {
 
   $(".lightbox-ordr").find("a").attr("title", "Technologies used: JavaScript, Express, EJS, Node.js, Postgres database hosted on Heroku, incorporated Twilio API");
   $(".lightbox-heavyvis").find("a").attr("title", "Technologies used: JavaScript, Three.js, Express, EJS, Node.js");
-
-  $('#heavyvis-swap').on("click", function() {
-    var src = $('#short').attr("src").replace("-short.gif", ".gif");
-    $('#short').attr("src", src);
-    $('#short').hide();
-    $('#short').fadeIn();
-    $(this).slideUp(500);
-
 })();
