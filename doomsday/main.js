a56dae458d99516de4e5090a8a34fee6c1ff9b49
Mario Viens <marviens@gmail.com>
2018-06-07 09:38:44 -0700

    Shaun +1 day

--- doomsday/main.js
+++ doomsday/main.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-const DOOMSDAY = new Date(Date.UTC(2018, 5, 8));
+const DOOMSDAY = new Date(Date.UTC(2018, 5, 9));
 
 document.addEventListener('DOMContentLoaded', () => {
   let clock = document.getElementById('doomsdayClock');
