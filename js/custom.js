948aeb68f28bf6cc1b3695c166d4e773a1c1e1bb
Mario Viens <marviens@gmail.com>
2022-01-16 18:26:37 -0800

    Remove old index page and associated assets

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-  var $lightbox1 = $('.lightbox-ordr a').simpleLightbox( { captionSelector: 'self'});
-  var $lightbox2 = $('.lightbox-heavyvis a').simpleLightbox( { captionSelector: 'self'});
-
-  $(".lightbox-ordr").find("a").attr("title", "Technologies used: JavaScript, Express, EJS, Node.js, Postgres database hosted on Heroku, incorporated Twilio API");
-  $(".lightbox-heavyvis").find("a").attr("title", "Technologies used: JavaScript, Three.js, Express, EJS, Node.js");
-})();
