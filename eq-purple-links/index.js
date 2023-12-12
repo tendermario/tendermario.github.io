85e99f1b15514076bc04f7af2152b98a696336fe
Mario Viens <marviens@gmail.com>
2023-12-12 10:35:34 -0800

    Barebones start to EQ purple links

---
+++ eq-purple-links/index.js
@@ -0,0 +1,13 @@
+r.onreadystatechange = function () {
+  var delimiter = document.getElementById('delimiter');
+  var inputList = document.getElementById('input-list');
+  var outputList = document.getElementById('output-list');
+  var prefix = document.getElementById('output-list');
+
+  // Goals:
+  // 1. For a given input list, create an output list
+  // 2. For an output list, modify it to have only the name and ID
+  // 3. For a name and ID, add purple link, ID hex and padding
+  // 4. For each purple link, add prefix and delimiter up to 255 characters separation
+  // 5. For each purple link list, add button prefix
+};
